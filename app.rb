f5cbdf328a7efaf916f7112d89bff9bdd71b96dd
christianf9156 <christianf9156@hstat.org>
2017-02-15 17:27:02 +0000

    Stuck on 1 failure

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+#https://github.com/christianf9156/07-passing-data-lab
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -10,8 +11,16 @@ class App < Sinatra::Base
     erb :reversed
   end
 
-  get '/friends' do
-    #write your code here
-  end
 
+
+  get '/friends' do
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
+  end
+  
+    post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    erb :reversed
+  end
 end
