25626c9a2d2e8e06449a0d54a83b63b6b0c707a2
Raquel Joseph <raquelj6437@hstat.org>
2019-02-11 03:44:39 +0000

    pass test

--- app.rb
+++ app.rb
@@ -1,17 +1,20 @@
 require_relative 'config/environment'
+# shotgun -p $PORT -o $IP
 
 class App < Sinatra::Base
-
+  post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    erb :reversed
+  end
+  
   get '/reverse' do
     erb :reverse
   end
-
-  post '/reverse' do
-    erb :reversed
-  end
+  
 
   get '/friends' do
-    #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
-
 end
